972137ad598e6ec62955aba93e91c9684f0fede9
slawless08 <samuel.l.lawless@gmail.com>
2021-07-07 16:48:59 -0400

    Testing confirmed that README will be generated given user answers.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ReadMeGenerator
+# hi
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./util/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -17,64 +18,74 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log("Created!"));
+}
 
 // TODO: Create a function to initialize app
 function init() {
+    console.log("Hello world!")
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'GitHubUsername',
                 message: questions[0],
-            }
+            },
             {
                 type: 'input',
                 name: 'GitHubLink',
                 message: questions[1],
-            }
+            },
             {
                 type: 'input',
                 name: 'email',
                 message: questions[2],
-            }
+            },
             {
                 type: 'input',
                 name: 'title',
                 message: questions[3],
-            }
+            },
             {
                 type: 'input',
                 name: 'description',
                 message: questions[4],
-            }
+            },
             {
                 type: 'input',
                 name: 'instsallation',
                 message: questions[5],
-            }
+            },
             {
                 type: 'input',
                 name: 'usage',
                 message: questions[6],
-            }
+            },
             {
                 type: 'input',
                 name: 'license',
                 message: questions[7],
-                choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-2-Clause', 'BSD-3-Clause'],
-            }
+                choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-2-Clause', 'BSD-3-Clause', 'none'],
+            },
             {
                 type: 'input',
                 name: 'contribute',
                 message: questions[8],
-            }
+            },
             {
                 type: 'input',
                 name: 'tests',
                 message: questions[9],
             }
         ])
+        .then((answers) => {
+            const myReadMe = generateMarkdown(answers);
+            console.log(answers);
+            writeToFile("README.MD", myReadMe);
+        })
+        
 }
 
 // Function call to initialize app
